9f75ad65ce65810c98426021808ba767ea0b4634
tomek <tomeksochacki@wp.pl>
2020-04-04 14:21:36 +0200

    Initialization repository

---
+++ src/com/test/Main.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TestConstructor testConstructor = new TestConstructor();
+
+        System.out.println(testConstructor.getOne());
+        System.out.println(testConstructor.getTwo());
+        System.out.println(testConstructor.getThree());
+        System.out.println(testConstructor.getFour());
+        System.out.println(testConstructor.getFive());
+        System.out.println(testConstructor.getSix());
+        System.out.println(testConstructor.getSeven());
+        System.out.println(testConstructor.getEight());
+        System.out.println(testConstructor.getReference());
+    }
+}
---
+++ src/com/test/TestConstructor.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import java.lang.ref.Reference;
+
+public class TestConstructor {
+    private byte one;
+    private short two;
+    private int three;
+    private long four;
+    private double five;
+    private float six;
+    private boolean seven;
+    private char eight;
+    Reference reference;
+
+    public Reference getReference(){
+        return reference;
+    }
+    public byte getOne(){
+        return one;
+    }
+    public short getTwo(){
+        return two;
+    }
+    public int getThree(){
+        return three;
+    }
+    public long getFour(){
+        return four;
+    }
+    public double getFive(){
+        return five;
+    }
+    public float getSix(){
+        return six;
+    }
+    public boolean getSeven(){
+        return seven;
+    }
+    public char getEight(){
+        return eight;
+    }
+
+}
